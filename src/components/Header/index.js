830e364fca237c45cac6f2d8809d0206ef4736cf
Noel Kearney <noelkearney01@outlook.com>
2022-10-19 15:17:09 +0100

    Homepage and header responsive

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,6 +7,7 @@ import Col from "react-bootstrap/esm/Col";
 import Row from "react-bootstrap/esm/Row";
 
 import './index.css';
+// import './sandpit.css';
 
 const MainHeader = () => {
     const [showNavBar, setShowNavBar] = useState(false);
@@ -16,32 +17,42 @@ const MainHeader = () => {
     }
 
     return (
-        <Container fluid>
-        
-        {showNavBar && <NavBar setShowNavBar={setShowNavBar} />}
-        {/* <div className='header-container'> */}
-        <Row className='homepage-header header-container'>
-        <Col>
-        <button className='nav-bar-header-btn' onClick={handleShowNavBar}>
-        <FontAwesomeIcon icon={faBars} />
-        </button>
-        </Col>
-    
-        <Col className='nav-bar-heder-center'>
-        <h1 className='nav-bar-header-title'>CUT 'N' SHAVE</h1>
-        </Col>
+        // <Container fluid>
 
-        {/* <Col className='d-flex justify-content-end'>
-        <button className='nav-bar-header-btn'>
-        <FontAwesomeIcon icon={faUser}/>
-        </button>
-        <button className='nav-bar-header-btn'>
-        <FontAwesomeIcon icon={faShoppingCart}/>
-        </button>
-        </Col> */}
-    </Row>
-    {/* </div> */}
-    </Container>
+        //     {showNavBar && <NavBar setShowNavBar={setShowNavBar} />}
+
+        //     <Row className='homepage-header'>
+
+        //         {/* <Col className='p-0'>
+        //             <button className='nav-bar-header-btn' onClick={handleShowNavBar}>
+        //                 <FontAwesomeIcon icon={faBars} />
+        //             </button>
+        //         </Col> */}
+
+        //         <Col className='nav-bar-header-center'>
+        //             <button className='nav-bar-header-btn' onClick={handleShowNavBar}>
+        //                 <FontAwesomeIcon icon={faBars} />
+        //             </button>
+        //             <h1 className='nav-bar-header-title'>CUT 'N' SHAVE</h1>
+        //         </Col>
+
+        //     </Row>
+        // </Container>
+        <>
+        {showNavBar && <NavBar setShowNavBar={setShowNavBar} />}
+
+        <Row className='homepage-header'>
+            {/* <Row > */}
+            <Col className='nav-bar-header'>
+                <button className='nav-bar-header-btn' onClick={handleShowNavBar}>
+                    <FontAwesomeIcon icon={faBars} />
+                </button>
+                <h1 className='nav-bar-header-title'>CUT 'N' SHAVE</h1>
+            </Col>
+            
+            {/* </Row> */}
+        </Row>
+    </>
     );
 }
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -36,146 +36,142 @@ const HomePage = () => {
         },
     ]);
 
-    const galleryPreviewImagesElements = galleryPreviewImagesData.map((img) => 
-        <img key={img.id} className='homepage-gallery-img' src={img.image} alt='gallery item'/>
+    const galleryPreviewImagesElements = galleryPreviewImagesData.map((img) =>
+        <img key={img.id} className='homepage-gallery-img' src={img.image} alt='gallery item' />
     );
 
     return (
-      <>
-      <MainHeader />
-       <Container fluid className='homepage-intro-container centered-container p-0'>
-        {/* <div className='homepage-intro-container centered-container'> */}
+        <>
+           
+            <Container fluid className='homepage-intro-container p-0'>
+                <MainHeader />
+            <Row className='centered-container homepage-intro-content '>
+                {/* <Row className='homepage-intro-title'> */}
+                    <Col className='centered-container'>
+                        <h1 className='homepage-intro-header'>FINEST CITY CENTER BARBERS</h1>
+                    </Col>
+                {/* </Row> */}
 
-            <Row className='homepage-intro-title'>
-                <Col>
-                <h1 className='homepage-intro-header'>FINEST CITY CENTER BARBERS</h1>
-                </Col>
-            </Row>
-            
-            <Row className='homepage-intro-btns-row'>
-            {/* className='homepage-intro-btns-col' */}
-                <Col sm='auto'>
-                    <Link className='white-btn link-btn' to='https://web.getsquire.com/' target='_blank'>BOOK AN APPOINTMENT</Link>
-                </Col>
-                <Col>    
-                    <Link className='white-btn link-btn' to='/contact_us'>CONTACT US</Link>
-                </Col>
-            </Row>
+                <Row className='btn-row'>
+                {/* className='homepage-intro-btns-col' */}
+                    <Col xs={6} className='homepage-intro-btns-col'>
+                        <Link className='white-btn link-btn' to='https://web.getsquire.com/' target='_blank'>BOOK AN APPOINTMENT</Link>
+                    </Col>
+                    <Col xs={6} className='homepage-intro-btns-col'>
+                        <Link className='white-btn link-btn' to='/contact_us'>CONTACT US</Link>
+                    </Col>
+                </Row>
+                </Row>
+            </Container>
 
-        {/* <Row className='homepage-intro-down-btn justify-content-sm-center '>
-            <Col sm='auto'>
-            <button className='homepage-down-btn'><FontAwesomeIcon icon={faAngleDown}/></button>
-            </Col>
-        </Row> */}
-        {/* </div> */}
-      
-        
-       </Container>
+            <Container className='homepage-brief-about-us-container'>
+                <Row>
+                    <Col className='text-center'>
+                        <span className='spacer-block'></span>
+                        <h2 className='bold-font'>A BRIEF ABOUT US</h2>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className='text-center homepage-brief-about-us-col'>
+                        <p>Ut vel risus ut leo sodales interdum. Fusce nunc lacus, cursus ut urna ut, condimentum porttitor massa. Etiam ut molestie ipsum, sit amet efficitur nulla. Integer accumsan, mi id molestie hendrerit, eros est dictum neque, quis rhoncus mi arcu vel erat. Mauris lobortis ex ac sapien fermentum commodo. Morbi et mattis tortor. Maecenas sed vulputate nisl. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+                        <p>Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer in orci a ligula placerat semper. Quisque diam dolor, facilisis ut eleifend sit amet, faucibus id ipsum. Duis nec malesuada ante. Phasellus eleifend libero risus, aliquam volutpat diam ornare a. Maecenas dui est, blandit id nulla at, tempor consequat eros. Aliquam vitae odio sed augue placerat vestibulum. Aenean interdum non nulla quis pellentesque.</p>
+                        <p>Sed sed mollis mi. Mauris eget justo turpis. Integer mollis lacinia elementum. Quisque sed urna nulla. In at neque ac metus imperdiet viverra sed at arcu. Suspendisse varius dignissim diam, ut accumsan urna facilisis at. Morbi eu purus dui.</p>
+                    </Col>
+                </Row>
+            </Container>
+            {/* sm={1} lg={2}  */}
+            <Container fluid >
 
-       <Container className='homepage-brief-about-us-container'>
-       <Row>
-            <Col className='text-center'>
-            <span className='spacer-block'></span>
-            <h2 className='bold-font'>A BRIEF ABOUT US</h2>
-            </Col>
-        </Row>
-        <Row>
-            <Col className='text-center homepage-brief-about-us-col'>
-            
-            <p>Ut vel risus ut leo sodales interdum. Fusce nunc lacus, cursus ut urna ut, condimentum porttitor massa. Etiam ut molestie ipsum, sit amet efficitur nulla. Integer accumsan, mi id molestie hendrerit, eros est dictum neque, quis rhoncus mi arcu vel erat. Mauris lobortis ex ac sapien fermentum commodo. Morbi et mattis tortor. Maecenas sed vulputate nisl. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
-            <p>Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer in orci a ligula placerat semper. Quisque diam dolor, facilisis ut eleifend sit amet, faucibus id ipsum. Duis nec malesuada ante. Phasellus eleifend libero risus, aliquam volutpat diam ornare a. Maecenas dui est, blandit id nulla at, tempor consequat eros. Aliquam vitae odio sed augue placerat vestibulum. Aenean interdum non nulla quis pellentesque.</p>
-            <p>Sed sed mollis mi. Mauris eget justo turpis. Integer mollis lacinia elementum. Quisque sed urna nulla. In at neque ac metus imperdiet viverra sed at arcu. Suspendisse varius dignissim diam, ut accumsan urna facilisis at. Morbi eu purus dui.</p>
-            </Col>
-        </Row>
-       </Container>
-       {/* sm={1} lg={2}  */}
-       <Container fluid >
+                <Row className='text-center'>
+                    
+                    <Col className='grey-square centered-container row-margin' md={6}>
+                        <h2 className='bold-font'>BOOK AN APPOINTMENT</h2>
+                        <p>Book an appointment here or navigate to Contact Us.</p>
+                        <Row className='btn-row'>
+                            <Col xs={6} className='homepage-intro-btns-col'>
+                            <Link className='black-btn link-btn'>BOOK AN APPOINTMENT</Link>
+                            </Col>
+                            <Col xs={6} className='homepage-intro-btns-col'>
+                            <Link className='black-btn link-btn' to='/contact_us'>CONTACT US</Link>
+                            </Col>
+                        </Row>
+                    </Col>
 
-        <Row className='text-center'>
-            <Col className='grey-square centered-container' md={6}>
-            <h2 className='bold-font'>BOOK AN APPOINTMENT</h2>
-            <p>Book an appointment here or navigate to Contact Us.</p>
-            <div>
-            <Link className='black-btn link-btn'>BOOK AN APPOINTMENT</Link>
-            <Link className='black-btn link-btn' to='/contact_us'>CONTACT US</Link>
-            </div>
-            </Col>
+                    <Col className='homepage-square-img-one centered-container' md={6}>
+                        <h2 className='bold-font'>OPENING TIMES</h2>
+                        <Row className='row'>
+                            <Col xs={6} className='font-bold'>
+                                <p>MONDAY...</p>
+                                <p>TUESDAY...</p>
+                                <p>WEDNESDAY...</p>
+                                <p>THURSDAY...</p>
+                                <p>FRIDAY...</p>
+                                <p>SATURDAY...</p>
+                                <p>SUNDAY...</p>
+                            </Col>
+                            <Col xs={6}>
+                                <p>08:00-17:00</p>
+                                <p>08:00-17:00</p>
+                                <p>08:00-17:00</p>
+                                <p>08:00-17:00</p>
+                                <p>08:00-17:00</p>
+                                <p>08:00-17:00</p>
+                                <p>CLOSED</p>
+                            </Col>
+                        </Row>
+                    </Col>
 
-            <Col className='homepage-square-img-one centered-container' md={6}>
-            <h2 className='bold-font'>OPENING TIMES</h2>
-            <Row className='row'>
-                <Col xs={6} className='font-bold'>
-                    <p>MONDAY...</p>
-                    <p>TUESDAY...</p>
-                    <p>WEDNESDAY...</p>
-                    <p>THURSDAY...</p>
-                    <p>FRIDAY...</p>
-                    <p>SATURDAY...</p>
-                    <p>SUNDAY...</p>
-                </Col>
-                <Col xs={6}>
-                   <p>08:00-17:00</p>
-                   <p>08:00-17:00</p>
-                   <p>08:00-17:00</p>
-                   <p>08:00-17:00</p>
-                   <p>08:00-17:00</p>
-                   <p>08:00-17:00</p>
-                   <p>CLOSED</p>
-                </Col>
-            </Row>
-            </Col>
+                    <Col className='square homepage-square-img-two centered-container' md={6}>
+                        <h2 className='bold-font'>PRODUCTS</h2>
+                        <p>Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer in orci a ligula placerat semper. Quisque diam dolor, facilisis ut eleifend sit amet, faucibus id ipsum. Duis nec malesuada ante. Phasellus eleifend libero risus, aliquam volutpat diam ornare a. Maecenas dui est, blandit id nulla at, tempor consequat eros. Aliquam vitae odio sed augue placerat vestibulum. Aenean interdum non nulla quis pellentesque.</p>
+                    </Col>
 
-            <Col className='square homepage-square-img-two centered-container' md={6}>
-            <h2 className='bold-font'>PRODUCTS</h2>
-            <p>Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer in orci a ligula placerat semper. Quisque diam dolor, facilisis ut eleifend sit amet, faucibus id ipsum. Duis nec malesuada ante. Phasellus eleifend libero risus, aliquam volutpat diam ornare a. Maecenas dui est, blandit id nulla at, tempor consequat eros. Aliquam vitae odio sed augue placerat vestibulum. Aenean interdum non nulla quis pellentesque.</p>
-            </Col>
+                    <Col className='homepage-square-img-three centered-container' md={6}>
+                        <h2 className='bold-font'>SERVICES</h2>
+                        <p>Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer in orci a ligula placerat semper. Quisque diam dolor, facilisis ut eleifend sit amet, faucibus id ipsum. Duis nec malesuada ante. Phasellus eleifend libero risus, aliquam volutpat diam ornare a. Maecenas dui est, blandit id nulla at, tempor consequat eros. Aliquam vitae odio sed augue placerat vestibulum. Aenean interdum non nulla quis pellentesque.</p>
+                        <div>
+                            <Link className='white-btn link-btn' to='/services'>VIEW SERVICES</Link>
+                        </div>
+                    </Col>
 
-            <Col className='homepage-square-img-three centered-container' md={6}>
-            <h2 className='bold-font'>SERVICES</h2>
-            <p>Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer in orci a ligula placerat semper. Quisque diam dolor, facilisis ut eleifend sit amet, faucibus id ipsum. Duis nec malesuada ante. Phasellus eleifend libero risus, aliquam volutpat diam ornare a. Maecenas dui est, blandit id nulla at, tempor consequat eros. Aliquam vitae odio sed augue placerat vestibulum. Aenean interdum non nulla quis pellentesque.</p>
-            <div>
-            <Link className='white-btn link-btn' to='/services'>VIEW SERVICES</Link>
-            </div>
-            </Col>
+                </Row>
+            </Container>
 
-        </Row>
-       </Container>
+            <Container fluid className='grey-background homepage-gallery-container' >
 
-       <Container fluid className='grey-background homepage-gallery-container' >
-       
-        <Row>
-            <Col className='text-center'>
-                <span className='spacer-block'></span>
-                <h2 className='bold-font'>GALLERY</h2>
-            </Col>
-        </Row>
-        
-        <Row>
-            <Col className="text-center">
-                {galleryPreviewImagesElements}
-            </Col>
-        </Row>
-        
-        <Row>
-            <Col className='homepage-brief-about-us-col'>
-                <Link className='black-btn margin-0 link-btn' to='/gallery'>VIEW MORE</Link>
-            </Col>
-        </Row>
+                <Row>
+                    <Col className='text-center'>
+                        <span className='spacer-block'></span>
+                        <h2 className='bold-font'>GALLERY</h2>
+                    </Col>
+                </Row>
 
-       </Container>
+                <Row>
+                    <Col className="text-center">
+                        {galleryPreviewImagesElements}
+                    </Col>
+                </Row>
 
-       <Container fluid >
-        <Row className='black-divider-container'>
-            <Col>
-            <button className='fit-black-btn'>BOOK AN APPOINTMENT</button>
-            </Col>
-        </Row>
-       </Container>
+                <Row>
+                    <Col className='homepage-brief-about-us-col'>
+                        <Link className='black-btn margin-0 link-btn' to='/gallery'>VIEW MORE</Link>
+                    </Col>
+                </Row>
 
-        <Footer />
-       
-        </> 
-        
+            </Container>
+
+            <Container fluid >
+                <Row className='black-divider-container'>
+                    <Col>
+                        <button className='fit-black-btn'>BOOK AN APPOINTMENT</button>
+                    </Col>
+                </Row>
+            </Container>
+
+            <Footer />
+
+        </>
+
     )
 }
 
